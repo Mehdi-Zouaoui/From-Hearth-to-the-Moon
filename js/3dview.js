20a6d62506949d978ad0ac376e7ec02a1944bf46
Mehdi <md1.zouaoui@gmail.com>
2019-05-10 15:01:00 +0200

    Upgrade on everthing , comments added , sun added for 3Dview , Voyage is working in real time

--- js/3dview.js
+++ js/3dview.js
@@ -1,90 +1,115 @@
 var camera, scene, renderer, controls;
 var mesh;
 let t = 1;
-let r  = 35;
+
+/* Création rayon Terre Lune */
+let rayon = 50;
 init();
 animate();
 moon_path();
 
 
 function init() {
-
+    /* Création de la scène */
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 5000);
+    
+    /* Permet de gerer le drag and drop */
     controls = new THREE.OrbitControls(camera);
-
-
-    //controls.update() doit être appeler obligatoirement apre sun changement manuel de la camera
+    /*controls.update() doit être appeler obligatoirement apre sun changement manuel de la camera*/
     controls.update();
+
     camera.position.set(0, 0, 100);
 
+    var light = new THREE.PointLight( 0xffffff, 2, 10000 );
+    var light_2 = new THREE.PointLight( 0xffffff, 2, 10000 );
+    var ambient_light = new THREE.AmbientLight( 0x606060 ); // soft white light
+    scene.add( light );
+    light.position.set( -10, 00, -50 );
+    light_2.position.set( 10, 30, -50 );
+    scene.add( light );
+    scene.add(ambient_light);
+    /* Création des plans pour le background */
     const plane_texture = new THREE.TextureLoader().load('images/stars-1920x1080.jpg');
     const plane_geometry = new THREE.PlaneGeometry(window.innerWidth, window.innerHeight, 32);
-    const plane_material = new THREE.MeshBasicMaterial({map: plane_texture, side: THREE.DoubleSide });
-    const random_material = new THREE.MeshBasicMaterial( {color: 0xffff00, side: THREE.DoubleSide} );
+    const plane_material = new THREE.MeshBasicMaterial({ map: plane_texture, side: THREE.DoubleSide });
+    //const random_material = new THREE.MeshBasicMaterial({ color: 0xffff00, side: THREE.DoubleSide });
+    
     const plane = new THREE.Mesh(plane_geometry, plane_material);
-    const plane_2 = new THREE.Mesh(plane_geometry,plane_material);
-    const plane_3 = new THREE.Mesh(plane_geometry,plane_material);
-    const plane_4 = new THREE.Mesh(plane_geometry,plane_material);
-    plane_2.rotation.y = Math.PI/2;
-    plane_3.rotation.y = Math.PI/2;
-    plane.position.set(20,0,-200);
-    plane_2.position.set(300,-50,200);
-    plane_3.position.set(-500,-50,200);
-    plane_4.position.set(20,0,500);
+    const plane_2 = new THREE.Mesh(plane_geometry, plane_material);
+    const plane_3 = new THREE.Mesh(plane_geometry, plane_material);
+    const plane_4 = new THREE.Mesh(plane_geometry, plane_material);
+    
+    plane_2.rotation.y = Math.PI / 2;
+    plane_3.rotation.y = Math.PI / 2;
+    plane.position.set(20, 0, -400);
+    plane_2.position.set(300, -50, 200);
+    plane_3.position.set(-500, -50, 200);
+    plane_4.position.set(20, 0, 500);
+    
     scene.add(plane);
     scene.add(plane_2);
     scene.add(plane_3);
     scene.add(plane_4);
 
+    /* Création de la Terre */
     const earth_texture = new THREE.TextureLoader().load('images/texture_earth-5400x2700.jpg');
     const earth_geometry = new THREE.SphereGeometry(10, 16, 16);
-    const earth_material = new THREE.MeshBasicMaterial({ map: earth_texture });
+    const earth_material = new THREE.MeshLambertMaterial({ map: earth_texture });
+
     earth = new THREE.Mesh(earth_geometry, earth_material);
     earth.position.set(0, 0, 0);
-    scene.add(earth);
 
+    scene.add(earth);
+    
+    const sun_texture = new THREE.TextureLoader().load('images/8k_sun.jpg');
+    const sun_geometry = new THREE.SphereGeometry(60, 16, 16);
+    const sun_material = new THREE.MeshLambertMaterial({ map: sun_texture });
+
+   sun = new THREE.Mesh(sun_geometry,sun_material);
+   sun.position.set(0, 150, -250);
+
+    scene.add(sun);
+    /* Création de la Lune */
     const moon_texture = new THREE.TextureLoader().load('images/texture_moon-2048x1024.jpg');
     const geometry = new THREE.SphereGeometry(5, 16, 16);
-    const material = new THREE.MeshBasicMaterial({ map: moon_texture });
+
+    const material = new THREE.MeshLambertMaterial({ map: moon_texture });
     moon = new THREE.Mesh(geometry, material);
     moon.position.set(0, 0, -60);
+
     scene.add(moon);
 
+    /* Création de renderer */
     renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
     //
-    
+
 
 }
 
-
-function moon_path(){
-  
-    moon.position.z = r*Math.sin(THREE.Math.degToRad(t));
-    moon.position.x = r*Math.cos(THREE.Math.degToRad(t));
+/* Fonction permettant à la lune de tourner */
+function moon_path() {
+    moon.position.z = rayon * Math.sin(THREE.Math.degToRad(t));
+    moon.position.x = rayon * Math.cos(THREE.Math.degToRad(t));
 }
 
 
 function animate() {
 
     t += 1;
+
     requestAnimationFrame(animate);
     controls.update();
-    //const date  = new Date() ;
-
-
-    console.log(r*Math.sin(THREE.Math.degToRad(t)));
-
+  
+    console.log(rayon * Math.sin(THREE.Math.degToRad(t)));
 
     earth.rotation.y += 0.01;
     moon.rotation.y += 0.01;
-    
-   
 
     renderer.render(scene, camera);
+
     moon_path();
 }
-   
--- js/voyage.js
+++ js/voyage.js
@@ -1,61 +1,55 @@
-let tab = [];
-
-let slider_data  = [
+/* Données imposées par le sujet */
+let slider_data = [
     {
-        nom : "homme à pied",
-        vitesse_min :0 ,
-        vitesste_max : 10.0
+        nom: "homme à pied",  
+        vitesse : 10.0
     },
     {
-        nom : "cheval pur-sang",
-        vitesse_min :10.1 ,
-        vitesste_max : 65.0
+        nom: "cheval pur-sang",      
+        vitesse: 65.0
     },
     {
-        nom : "voiture ferrari",
-        vitesse_min :65.1,
-        vitesste_max : 325
+        nom: "voiture ferrari",       
+        vitesse: 325
     },
     {
-        nom : "avion jet",
-        vitesse_min :325.1 ,
-        vitesste_max : 740
+        nom: "avion jet",        
+        vitesse: 740
     },
     {
-        nom : "avion de ligne A380",
-        vitesse_min :740.1 ,
-        vitesste_max :1185
+        nom: "avion de ligne A380",        
+        vitesse: 1185
     },
     {
-        nom : "fusée Apollo 11",
-        vitesse_min :1185.1 ,
-        vitesste_max : 5500
-    }   
+        nom: "fusée Apollo 11",
+        vitesse: 5500
+    }
 
 ]
 
 
-
-
-
-function data_research(user_choice){
+/* Fonction qui compare la valeure choisie par l'utilisateur 
+    avec les valeurs de vitesse de chaque véhicules
+    afin d'associer la vitesse choisis au véhicule trouvé*/
+function data_research(user_choice) {
     console.log(user_choice);
-    for( let i = 0 ; i <slider_data.length ; i++){
+    for (let i = 0; i < slider_data.length; i++) {
+        if (i!==slider_data.length - 1) {
 
-        for (let clé in slider_data[i]){
+            if (user_choice >= slider_data[i].vitesse && user_choice <= slider_data[i+1].vitesse /2) {
+                console.log("YO");
+                return slider_data[i].nom;
+                }
+                else if ( user_choice >= slider_data[i+1].vitesse /2 && user_choice <=slider_data[i+1].vitesse){
+                return slider_data[i+1].nom ;
+                }else continue ;
 
-            if(user_choice >= slider_data[i].vitesse_min && user_choice <= slider_data[i].vitesste_max){
-                
-            console.log(slider_data[i].nom);
-           return (slider_data[i].nom);
-        }
-        
-
-        }
+        }else slider_data[i].nom;
     }
 }
 
-
+/* Fonction qui convertit une donnée de temps en milliseconds 
+    en heure , minutes ,secondes */
 function msToTime(milliseconds) {
     //Get hours from milliseconds
     let hours = milliseconds / (1000 * 60 * 60);
@@ -76,7 +70,6 @@ function msToTime(milliseconds) {
 }
 
 
-
 const distance = 384400;
 const Ms_in_one_hour = 3600000;
 const slider = document.getElementById("myRange");
@@ -86,17 +79,23 @@ const timer = document.getElementById('time');
 const vehicules = document.getElementById('vehicules')
 output.innerHTML = slider.value;
 
-slider.oninput = function() {
-output.innerHTML = this.value;
-user_choice = this.value;
 
+/*  Slider interface , permet à l'utilisateur 
+    de choisir une vitesse en km/h */
+slider.oninput = function () {
+    output.innerHTML = this.value;
+    user_choice = this.value;
+    console.log(msToTime(user_choice));
+    timer.innerHTML = msToTime((distance / user_choice) * Ms_in_one_hour);
+    vehicules.innerHTML = data_research(user_choice);
+    
 }
-test_button.onclick = function (){
-console.log(msToTime(user_choice));
-timer.innerHTML = msToTime((distance/user_choice)*Ms_in_one_hour);
-vehicules.innerHTML = data_research(user_choice);
 
-}
+
+/* Quand on click sur le test_button
+    affichage des valeurs de temps 
+    et du véhicule trouvé   */
+
 
 
 
